4c8f737ecc33040119baa22a0564e693c1cfd9e4
lazing-wizard <lazing-wizard@gmail.com>
2025-09-22 13:16:35 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,33 +4,36 @@ import globals from 'globals';
 import js from '@eslint/js';
 import jest from 'eslint-plugin-jest';
 import prettier from 'eslint-config-prettier';
+import stylistic from '@stylistic/eslint-plugin';
 import eslintImport from 'eslint-plugin-import';
 
 /** @type {import('eslint').FlatConfig[]} */
 export default [
-  { ignores: ['dist/', 'coverage/', 'node_modules/'] },
+  { ignores: ['**/*', '!source/**', '!examples/**', '!scripts/**', '!tests/**'] },
   js.configs.recommended,
   {
-    files: ['**/*.js'],
+    files: ['source/**/*.js', 'examples/**/*.js', 'scripts/**/*.js', 'tests/**/*.js'],
     plugins: {
-      import: eslintImport,
+      stylistic: stylistic,
+      import: eslintImport
     },
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
     },
     rules: {
-      quotes: ['error', 'single', { allowTemplateLiterals: true, avoidEscape: false }],
-      'max-len': ['error', { code: 120, tabWidth: 2 }],
-      semi: 'error',
+      'stylistic/quotes': ['error', 'single', { allowTemplateLiterals: 'always', avoidEscape: false }],
+      'stylistic/max-len': ['error', { code: 120, tabWidth: 2 }],
+      'stylistic/semi': 'error',
+      'stylistic/indent': ["error", 2],
       'prefer-const': 'warn',
       'no-unused-vars': 'warn',
-      'no-constructor-return': 'error',
+      'no-constructor-return': 'off',
       'no-duplicate-imports': 'error',
       'no-inner-declarations': 'error',
       'no-self-compare': 'error',
       'no-unmodified-loop-condition': 'error',
-      'no-use-before-define': 'warn',
+      'no-use-before-define': 'off',
       'no-useless-assignment': 'error',
       'import/no-unresolved': 'error',
       'import/extensions': ['error', 'ignorePackages'],
